30d577e644ef4e66154e7d5300f040b158aa1b11
ArielAVega <avega@fi.unju.edu.ar>
2024-12-13 17:06:05 -0300

    Generacion y movimiento de la nave
    Se crea el game object para la nave, se le asocia un sprite y el script para generar un moviemiento delimitado por los bordes de la pantalla

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _velocity;
+    void Start()
+    {
+        _velocity = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        float inputH = Input.GetAxisRaw("Horizontal");
+        float inputV = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector2(inputH, inputV).normalized * _velocity * Time.deltaTime);
+
+        float xClamped = Mathf.Clamp(transform.position.x, -8.41f, 8.41f);
+        float yClamped = Mathf.Clamp(transform.position.y, -4.46f, 4.46f);
+        transform.position = new Vector2(xClamped, yClamped);   
+
+
+    }
+}
